9059c7b0ebd97fdebff5092c90e70ee0c3dded24
Alex Ausch <alex.ausch@gmail.com>
2013-10-23 11:07:15 -0400

    typo

--- javascripts/github.js
+++ javascripts/github.js
@@ -14,8 +14,6 @@ thisModule.addSlots(avocado.github, function(add) {
 
   add.creator('file', {});
 
-  add.creator('_github', Object.create({}));
-
   add.method('login', function (uid, pwd, callback) {
     if (uid && pwd) {
       avocado.github._github = new Octokit({
@@ -29,6 +27,8 @@ thisModule.addSlots(avocado.github, function(add) {
   });
 
   add.data('_current_repo', null);
+  
+  add.data('_github', null);
 
   add.method('github', function () {
     if (this._github) { return this._github; }
